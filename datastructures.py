0c72cee303e321b06540e2591895f9fa35f950f2
Tejas199818 <54523003+Tejas199818@users.noreply.github.com>
2019-09-14 21:43:09 +0530

    Update datastructures.py

--- datastructures.py
+++ datastructures.py
@@ -10,7 +10,7 @@ def my_list():
     print("Lets pop out Kawasaki since it's not in my budget\n",bike)
     bike.sort()
     print("My bike list in order\n",bike)
-    
+my_list()
 def my_tuple():
     game=('COD','PUBG','CS-GO')
     print("This is my tuple\n",game)
@@ -18,4 +18,41 @@ def my_tuple():
     game+=('DOTA',)
     print("Let me add DOTA to my tuple",game)
     print("There are",len(game),"games in my tuple")
-    
+my_tuple()    
+def abc2():
+    print("DICTIONARY:")
+    a={0:"Konichiwa",1:"Ohaiyo",2:"Kawaii",3:"Baka",4:"konoyaro",5:"Utsukushi",6:"Arigatho"}
+    inp=int((input("Enter a number from 0 to 6\n")))
+    print(a[inp])
+    print("\n\n")
+
+abc2()
+
+
+
+def abc3():
+    print("TUPLES:")
+    print("Tuples are immutable")
+    a=('too','koo','poo')
+    print(a)
+    print("\n\n")
+abc3()
+  
+
+
+def abc4():
+    print("SETS:")
+    a={0,1,2}
+    b={2,3,4}
+    print("Intersection",a&b)
+    print()
+    print("Union",a|b)
+    print()
+    print("A==B?",a==b)
+    print()
+    print("A-B",a-b)
+    print()
+    print("B-A",b-a)
+    print()
+    print("\n\n")
+abc4() 
